31465c6e415d90ddb34a0cc5988fef9d94a89d56
jaishruti <shrutijaiswal223@gmail.com>
2025-01-16 20:21:07 +0530

    alien group formation for 3 different types of aliens

--- main.py
+++ main.py
@@ -2,9 +2,20 @@ import pygame
 import random
 from Shooter import Shooter
 from bullet import Bullet
+from alien import Alien
 bullets = pygame.sprite.Group()
+aliens = pygame.sprite.Group()
 
-            
+def alien_setup(rows,cols,x_distance = 60,y_distance = 48,x_offset = 70, y_offset = 100):   
+            for row_index, row in enumerate(range(rows)):
+                for col_index, col in enumerate(range(cols)):
+                    x = col_index * x_distance + x_offset
+                    y = row_index * y_distance + y_offset
+                    
+                    if row_index == 0: alien_sprite = Alien('Alien1',x,y)
+                    elif 1 <= row_index <= 2: alien_sprite = Alien('Alien2',x,y)
+                    else: alien_sprite = Alien('bug',x,y)
+                    aliens.add(alien_sprite)
 def game():
     win_height = 600
     win_width = 800
@@ -16,11 +27,12 @@ def game():
     #create shooter object
     player = Shooter((win_width//2,win_height-30),(s,s))
     run = True
+                        
     def fire_bullet():  
             bulletOb = Bullet('player',player.rect.center)
             bullets.add(bulletOb)
             print(len(bullets))
-            
+    alien_setup(rows=6, cols=8)
     clock = pygame.time.Clock()
     #main game loop
     while run:
@@ -28,10 +40,17 @@ def game():
         
         win.blit(bg,(0,0))
         win.blit(player.surf, player.rect)
+
+        #appear bullet
         for bulletOb in bullets:
              win.blit(bulletOb.surf,bulletOb.rect)
              bulletOb.move()
 
+        #move aliens
+        for alien in aliens:
+            win.blit(alien.surf,alien.rect)
+            alien.update()
+            
         #close win on Quit button click
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
