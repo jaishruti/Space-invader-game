c51f76b49c4b24fa106e2a0375538fd9ff7ccfde
jaishruti <shrutijaiswal223@gmail.com>
2025-01-16 18:45:01 +0530

    background and player added

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from Shooter import Shooter
+
+def game():
+    win_height = 600
+    win_width = 800
+    win = pygame.display.set_mode(size=(win_width,win_height))
+    bg = pygame.image.load('./assets/background.png')
+    #size of each object
+    s = 50
+    
+    #create shooter object
+    player = Shooter((win_width//2,win_height-30),(s,s))
+    run = True
+
+    #main game loop
+    while run:
+        win.blit(bg,(0,0))
+        win.blit(player.surf, player.rect)
+
+        #close win on Quit button click
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+        pygame.display.flip()
+
+game()
